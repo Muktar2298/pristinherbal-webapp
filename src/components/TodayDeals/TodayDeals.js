ec10a886f0fa8858035c33bfadccd5c4171a40c8
Md.Muktar Ali <muktar.webdev@gmail.com>
2022-11-08 11:42:27 +0600

    Adding New Features

--- src/components/TodayDeals/TodayDeals.js
+++ src/components/TodayDeals/TodayDeals.js
@@ -4,17 +4,20 @@ import { FaArrowRight } from 'react-icons/fa';
 
 import products from './../../fakeData/todayDealsData';
 import TodayDeal from '../TodayDeal/TodayDeal';
+import { Button } from 'react-bootstrap';
 
 const TodayDeals = () => {
   return (
     <div className="today-deals-container">
       <div className="today-deals-title">
-        <p>Popular Categories</p>
+        <p>Deals Deals</p>
+        {/* TIMER-ADDING */}
+        <div>
+          <p>Ending In</p>
+          <div></div>
+        </div>
         <p>
-          <span style={{ marginRight: '0.3rem' }}>Browser All Categories </span>
-          <span>
-            <FaArrowRight />
-          </span>
+          <button type="submit">SHOP MORE</button>
         </p>
       </div>
       {/* <hr className="horizontal" /> */}
