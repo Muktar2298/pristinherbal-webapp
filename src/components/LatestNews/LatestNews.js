629941668f63598160a10addda56eb2718ee44fe
Md.Muktar Ali <muktar.webdev@gmail.com>
2022-11-08 13:11:08 +0600

    LatestNews:: Implemented

--- src/components/LatestNews/LatestNews.js
+++ src/components/LatestNews/LatestNews.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './LatestNews.css';
 import { FaStar } from 'react-icons/fa';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 const LatestNews = (props) => {
+  const [modalShow, setModalShow] = React.useState(false);
+
   const news = props.news;
   const { description } = news;
   const str = description.slice(1, 150);
@@ -15,10 +20,45 @@ const LatestNews = (props) => {
         <h6 className="news-name">{news.name}</h6>
         {/* <h6 className="product-price"> ৳ {product.price}</h6> */}
         <p className="news-description">{str}...</p>
-        <button>Read More</button>
+        <button onClick={() => setModalShow(true)}>Read More</button>
+      </div>
+      <div>
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          news={news}
+        />
       </div>
     </div>
   );
 };
 
 export default LatestNews;
+
+function MyVerticallyCenteredModal(props) {
+  const { name, img, description } = props.news;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header >
+        <div>
+          <img src={img} alt=""  />
+        </div>
+        {/* <Modal.Title id="contained-modal-title-vcenter">
+          {name}
+        </Modal.Title> */}
+      </Modal.Header>
+      <Modal.Body>
+        <h4>{name}</h4>
+        <p>{description}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='modal-btn' onClick={props.onHide}>Close</button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
